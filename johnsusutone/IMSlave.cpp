95af4de7a576cc679302141d2b4e16ba4fa3e5ca
chicken2039 <zoq2039@gmail.com>
2016-11-06 15:12:36 +0900

    ang

--- johnsusutone/IMSlave.cpp
+++ johnsusutone/IMSlave.cpp
@@ -1,9 +1,81 @@
 ﻿#include "func.h"
 
+void initLinkedList(void);
+
+
 int main()
 {
-	struct block Block[3][7];
+	initLinkedList();
+	char Command[50];						//명령어 변수
+
+	struct block Block[4][7]; //블록 선언
+	
+	int game_Start;
+		
+	for (int i = 0; i < 4; i++)				//블럭 구조체에 커서값 부여
+	{
+		for (int j = 0; j < 7; j++)
+		{
+			Block[i][j].x_eco=(i*8)+3;
+			Block[i][j].y_eco=(i*4)+6;
+			Block[i][j].x_unit = (i * 8) + 3;
+			Block[i][j].y_unit = (i * 4) + 6+1; 
+			Block[i][j].x_resor = (i * 8) + 3;
+			Block[i][j].y_resor = (i * 4) + 6 + 2;
+		}
+	}
+
+	struct HQ My;		//적과 나 헤드쿼터 구조체 생성
+	struct HQ Enemy;
+	My.x1 = Block[3][3].x_eco;
+	My.y1 = Block[3][3].y_eco;
+	My.x2 = Block[3][4].x_eco;
+	My.y2 = Block[3][4].y_eco;
+
+	Enemy.x1 = Block[0][3].x_eco;
+	Enemy.y1 = Block[0][3].y_eco;
+	Enemy.x2 = Block[0][4].x_eco;
+	Enemy.y2 = Block[0][4].y_eco;
+
+
+	printf("게임시작(1),종료(그 이외):");
+	scanf("%d", &game_Start);
+
+	if (game_Start != 1)
+	{
+		return 0;
+	}
+	system("cls");
+	
+	
+
+
 	printmap();
+	for(int i=1;1;i++)
+	{
+		{
+			printf("");
+		}
+		if (i%2==0)//내턴일때
+		{
+			gotoxy(8, 26);
+			fflush(stdin);
+			fgets(Command, 50, stdin);		//명령어 받기
+			gotoxy(8, 26);
+			printf("　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　");// 줄 비우기
+
+
+		}
+
+		else //상대 턴 일떄
+		{
+			Sleep(1000);		//상대 턴일때 텀을줌
+
+								//인공지능 들어가야함
+			getch();
+		}
+
+	}
 
 
 
@@ -11,3 +83,24 @@ int main()
 
 }
 
+typedef struct unit //유닛
+{
+	char unit_Name[7];
+	int x,y;				//유닛위치
+	int atk;				//공격력
+	int hp;					//채력
+	BOOL AbleGather;		//채광가능 여부
+	int MoveAble;			//움직임 가능?
+	struct unit *next;		//다음 유닛
+	struct unit *before;	//이전 유닛
+}unit;
+
+void initLinkedList(void)//head,tail 생성
+{
+	unit *head=(unit*)malloc(sizeof(unit));
+	unit *tail=(unit*)malloc(sizeof(unit));
+	head -> next=tail;
+
+	tail -> next=NULL;
+	
+}
--- johnsusutone/func.h
+++ johnsusutone/func.h
@@ -3,25 +3,50 @@
 #include <conio.h>
 #include <malloc.h>
 #include <Windows.h>
+#include <string>
 
-struct block
+
+
+void gotoxy(int x, int y) {//커서 옮기기
+	COORD Pos = { x - 1, y - 1 };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
+}
+
+struct HQ //본거지
+{
+	int x2, y2;
+	int x1, y1;
+	int HQ_hp;
+	char HQ_name[10];
+	
+
+};
+
+struct block //블럭
 {
 	int x_eco, y_eco;
 	int x_unit, y_unit;
 	int x_resor, y_resor;
+	BOOL summonAble;
 };
+void Apply_Command(char command[])
+{
 
-
-
-
-void gotoxy(int x, int y) {
-	COORD Pos = { x - 1, y - 1 };
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
 }
-void printmap() {
+
+
+void print_OnBoard(int x , int y,char ang[] ) //환경 출력
+{
+	gotoxy(x, y);
+	printf("%s", ang);
+	gotoxy(8, 26);//다시 명령줄로 돌아감
+	
+}
+
+void printmap() {				//기본 맵
 
 	puts("│　　　　　　　　　　　　　┌───┐　　　　　　　　　　　　　　　　　　　　");
-	puts("│　　　　　　　　　　　　　│김준수│　　　　　　　　　　　　　┌──────");
+	puts("│　　　　　　　　　　　　　│킹존스│　　　　　　　　　　　　　┌──────");
 	puts("│　　　　　　　　　　　　　│　전사│　　　　　　　　　　　　　│　　　　　　");
 	puts("│　　　　　　　　　　　　　└───┘　　　　　　　　　　　　　│　　　　　　");
 	puts("├───┬───┬───┬───┬───┬───┬───┬───┤　　　　　　");
@@ -42,9 +67,10 @@ void printmap() {
 	puts("│　　　│　　　│　　　│　　　│　　　│　　　│　　　│　　　│　　　　　　");
 	puts("├───┴───┴───┴───┴───┴───┴───┴───┤　　　　　　");
 	puts("│　　　　　　　　　　　　　┌───┐　　　　　　　　　　　　　│　　　　　　");
-	puts("│　　　　　　　　　　　　　│안용호│　　　　　　　　　　　　　│　　　　　　");
+	puts("│　　　　　　　　　　　　　│카페인│　　　　　　　　　　　　　│　　　　　　");
 	puts("│　　　　　　　　　　　　　│　돚거│　　　　　　　　　　　　　│　　　　　　");
 	puts("│　　　　　　　　　　　　　└───┘　　　　　　　　　　　　　└──────");
+	puts("│명령:");
 
 
 }
